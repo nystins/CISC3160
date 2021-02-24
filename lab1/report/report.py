d4541d1320a48e80df82ef6548f22f726209847d
nystins <zomgitzjohn@gmail.com>
2021-02-24 08:28:08 -0500

    updated report.py

--- lab1/report/report.py
+++ lab1/report/report.py
@@ -1,29 +1,14 @@
-import csv
-import operator
+import pandas as pd
 
-# function for getting the unique artist list
-def unique(list):
-  list_of_unique_artist = []
-  # insert the list to the set
-  list_set = set(list)
-  for unique_list in list_set:
-    list_of_unique_artist.append(unique_list)
-  return list_of_unique_artist
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
 
-with open('regional-global-weekly-latest.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    artist_list = []
-    for row in csv_reader:
-        if line_count == 0:
-          line_count += 1
-        else:
-          # csv file heading
-          # row[0] = position, row[1] = Track Name, row[2] = Artist Name, row[3] = Streams, row[4] = URL
-          # get artist name
-          artist_list.append(row[2])
-          # get the average stream per song
-          print(row[1], int(row[3]) / 7)
-          line_count += 1
-    print("----------------------Unique artist name-----------------------")
-    print(unique(artist_list))
+df = pd.read_csv('regional-global-weekly-latest.csv')
+#groupbuy is to get the artist unique (same logic as SQL)
+#.agg = allows to count how many artist are there, and sum up streams | aggregations
+#reset_index because after we deleted the duplicates original CSV is 0-199, it gets less after deletion
+df = df.groupby('Artist').agg({'Artist':'count', 'Streams':'sum'}).rename(columns={'Artist':'Count_dups'}).reset_index()
+df['Avg.Streams'] = df['Streams'] / df['Count_dups'] #to get the average streams divide by the number of duplicates 
+del df['Count_dups'] #deleted count_dups to not show in print
+print(df)
